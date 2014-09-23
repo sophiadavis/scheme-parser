ac1bf6e384564e6cc021e99eafcd793beb8fc8ca
Thomas Ballinger <thomasballinger@gmail.com>
2014-09-23 15:05:05 -0400

    eval including procedure application
    no scopes yet

--- interpreter.py
+++ interpreter.py
@@ -1,11 +1,11 @@
 # ()
+import operator
 
 import re
 
 
 def tokenize(s):
-    return re.findall(r"[()]|\w+", s)
-
+    return re.findall(r"[()]|[^\s()]+", s)
 
 def read(s):
     tokens = tokenize(s)
@@ -25,4 +25,33 @@ def read_tokens(tokens):
         else:
             l.append(current)
 
+def scheme_sub(*args):
+    if not args:
+        raise TypeError("expected 1 or more arguments")
+    elif len(args) == 1:
+        return -args[0]
+    else:
+        return reduce(operator.sub, args[1:], args[0])
+
+default_env = {
+        '+': lambda *args: sum(args),
+        '-': scheme_sub
+        }
+
 def eval(ast):
+    if isinstance(ast, list):
+        evaled = [eval(x) for x in ast]
+        return evaled[0](*evaled[1:])
+    elif isinstance(ast, str) and ast.isdigit():
+        return int(ast)
+    elif isinstance(ast, str) and ast in default_env:
+        return default_env[ast]
+    else:
+        raise ValueError(repr(ast))
+
+def repl():
+    while True:
+        print eval(read(raw_input()))
+
+if __name__ == '__main__':
+    repl()
--- interpreter_test.py
+++ interpreter_test.py
@@ -2,17 +2,25 @@ import unittest
 import interpreter
 
 class TestParse(unittest.TestCase):
-	def test_it_tokenizes_expressions(self):
-		self.assertEquals(interpreter.tokenize('(1 2 32)'), ["(", "1", "2", "32", ")"])
+    def test_it_tokenizes_expressions(self):
+        self.assertEquals(interpreter.tokenize('(1 2 32)'), ["(", "1", "2", "32", ")"])
+        self.assertEquals(interpreter.tokenize('(+ 2 32)'), ["(", "+", "2", "32", ")"])
 
-	def test_it_parses_lists(self):
-		self.assertEquals(interpreter.read('(1 2 3)'), ["1", "2", "3"])
+    def test_it_parses_lists(self):
+        self.assertEquals(interpreter.read('(1 2 3)'), ["1", "2", "3"])
 
-	def test_it_parses_nested_lists(self):
-		self.assertEquals(interpreter.read('(1 2 (23 hello (another list 43)))'), ["1", "2", ["23", "hello", ["another", "list", "43"]]])
+    def test_it_parses_application(self):
+        self.assertEquals(interpreter.read('(+ 1 2)'), ["+", "1", "2"])
 
-	def test_it_adds(self):
-		self.assertEquals(interpreter.eval('(+ 4 3)'), 7)
+    def test_it_parses_nested_lists(self):
+        self.assertEquals(interpreter.read('(1 2 (23 hello (another list 43)))'), ["1", "2", ["23", "hello", ["another", "list", "43"]]])
+
+    def test_it_adds(self):
+        self.assertEquals(interpreter.eval(['+', '4', '3']), 7)
+        self.assertEquals(interpreter.eval(['-', '4', '3', '2']), -1)
+        self.assertEquals(interpreter.eval(['-', '4']), -4)
+        with self.assertRaises(TypeError):
+            interpreter.eval(['-'])
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
